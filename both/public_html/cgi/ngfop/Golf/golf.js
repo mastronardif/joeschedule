c646d47fa94671a5d3ec2012d8a5214ce4292ea3
Frank Mastronardi <mastronardif@gmail.com>
2025-01-01 11:54:58 -0500

    move header id to js

--- both/public_html/cgi/ngfop/Golf/golf.js
+++ both/public_html/cgi/ngfop/Golf/golf.js
@@ -1,12 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
     const header = document.getElementById("golfheader");
     if (header) {
+      header.innerHTML = `
+                  <h1 class="header-title" onclick="loadPage('./indexgv22.html')" style="cursor: pointer;">
+                LinK
+            </h1>
+    `;
+
       header.onclick = function () {
         loadPage('./indexgv22.html');
       };
     }
+
+        // Populate the footer
+        const footer = document.getElementById("golffooter");
+        if (footer) {
+          footer.innerHTML = `
+            <p class="footer-text">&copy; Golfy Jones Inc. 2025</p>
+          `;
+        }
   });
-  
+
   function loadPage(url) {
     window.location.href = url;
   }
