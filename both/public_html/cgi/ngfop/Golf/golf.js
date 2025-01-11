110602f2c622e9a4f44dfa6cbda289e39509262d
Frank Mastronardi <mastronardif@gmail.com>
2025-01-11 10:05:15 -0500

    link now

--- both/public_html/cgi/ngfop/Golf/golf.js
+++ both/public_html/cgi/ngfop/Golf/golf.js
@@ -1,27 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const header = document.getElementById("golfheader");
-    if (header) {
-      header.innerHTML = `
+  const header = document.getElementById("golfheader");
+  if (header) {
+    header.innerHTML = `
                   <h1 class="header-title" onclick="loadPage('./indexgv22.html')" style="cursor: pointer;">
                 LinK
             </h1>
     `;
 
-      header.onclick = function () {
-        loadPage('./indexgv22.html');
-      };
-    }
+    header.onclick = function () {
+      loadPage("./indexgv22.html");
+    };
+  }
 
-        // Populate the footer
-        const footer = document.getElementById("golffooter");
-        if (footer) {
-          footer.innerHTML = `
+  const titleElement = document.getElementById("linktitle");
+  if (titleElement) {
+    titleElement.textContent = "Link";
+  }
+
+  // Populate the footer
+  const footer = document.getElementById("golffooter");
+  if (footer) {
+    footer.innerHTML = `
             <p class="footer-text">&copy; Golfy Jones Inc. 2025</p>
           `;
-        }
-  });
-
-  function loadPage(url) {
-    window.location.href = url;
   }
-  
+});
+
+function loadPage(url) {
+  window.location.href = url;
+}
---
+++ both/public_html/cgi/ngfop/Golf/test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+print("Content-type: text/html\n")  # CGI header
+print("<html><head><title>Hello Python CGI</title></head><body>")
+print("<h1>Hello, Python World!</h1>")
+
+print("<h1>Last Modified: July 30 12024!</h1>")
+print("</body></html>")
